a23c5d4d1060709b83ef924f2972f7f3707b35c1
Jonas Anso <janso@elmar.nl>
2014-01-11 14:57:28 +0000

    Removed some not compiling code

--- src/test/scala/samples/scalatest.scala
+++ src/test/scala/samples/scalatest.scala
@@ -75,32 +75,3 @@ class ListSuite extends FunSuite with ShouldMatchers {
   }
 }
 
-/*
-ScalaTest also supports the behavior-driven development style, in which you
-combine tests with text that specifies the behavior being tested. Here's
-an example whose text output when run looks like:
-
-A Map
-- should only contain keys and values that were added to it
-- should report its size as the number of key/value pairs it contains
-*/
-import org.scalatest.Spec
-import org.scalatest.matchers.MustMatchers
-
-class MapSpec extends Spec with MustMatchers {
-
-  describe("A Map") {
-
-    it("should only contain keys and values that were added to it") {
-      Map("ho" -> 12) must (not contain key ("hi") and not contain value (13))
-      Map("hi" -> 13) must (contain key ("hi") and contain value (13))
-    }
-
-    it("should report its size as the number of key/value pairs it contains") {
-      Map() must have size (0)
-      Map("ho" -> 12) must have size (1)
-      Map("hi" -> 13, "ho" -> 12) must have size (2)
-    }
-  }
-}
-
