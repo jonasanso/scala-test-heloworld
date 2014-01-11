4daaf9c306697986a73d1c9643b383474715c247
Jonas Anso <janso@elmar.nl>
2014-01-11 14:56:31 +0000

    Make hello world a bit testeable.

--- src/main/scala/org/hackthetower/samples/HelloWorld.java
+++ src/main/scala/org/hackthetower/samples/HelloWorld.java
@@ -3,9 +3,13 @@ package org.hackthetower.samples;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.sayHello());
     }
 
+    public String sayHello() {
+        return "Hello world";
+    }
     public Integer sum(Integer a, Integer b) {
         return a + b;
     }
--- src/test/scala/org/hackthetower/samples/HelloWorldTest.scala
+++ src/test/scala/org/hackthetower/samples/HelloWorldTest.scala
@@ -1,9 +1,15 @@
 package org.hackthetower.samples
 
-import org.scalatest.Matchers
+import org.scalatest.{FunSuite, Matchers}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
-class HelloWorldTest extends Matchers {
+@RunWith(classOf[JUnitRunner])
+class HelloWorldTest extends FunSuite with Matchers {
 
+  test("Should say hello") {
+    new HelloWorld().sayHello() shouldBe "Hello world"
+  }
   test("Should sum correctly 2 simple numbers") {
     new HelloWorld().sum(2, 3) shouldBe 5
   }
